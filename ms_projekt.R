fb7da8600d845fc4cad55f69997aa4bf56ebd0ac
wojciechsi <redocean@protonmail.com>
2022-04-21 16:14:23 +0200

    drugie posiedzenie

--- ms_projekt.R
+++ ms_projekt.R
@@ -27,7 +27,6 @@ moda_koszty <- moda(data1[['koszty']])
 print(moda_koszty)
 
 mediana_koszty <- median(data1[['koszty']])
-
 print(mediana_koszty)
 
 
@@ -41,3 +40,37 @@ print(wariancja_koszty)
 
 srednie_odch_bezwzg_koszty <- mad(data1[['koszty']])
 print(srednie_odch_bezwzg_koszty)
+
+wsp_zmiennosci_koszty <- sd(data1[['koszty']]) / mean(data1[['koszty']]) * 100
+print(wsp_zmiennosci_koszty)
+
+rozstep_koszty <- IQR(data1[['koszty']])
+print(rozstep_koszty)
+
+rozstep_cwiartkowy_koszty <- IQR(data1[['koszty']]) / 2
+print(rozstep_cwiartkowy_koszty)
+
+#MIARY ASYMETRII
+# @todo jakiśtam sizeof zamiast 25
+# @todo funkcja
+moment_centr_tmp <- 0
+for (xi in 1:25) {
+  moment_centr_tmp <- moment_centr_tmp + (data1[xi, "koszty"] - mean(data1[['koszty']]))^3
+}
+moment_centr_koszty <- 1/24 * moment_centr_tmp
+
+wspolczynnik_asymetrii_koszty <- (moment_centr_koszty) / (odchylenie_koszty ^ 3)
+print(wspolczynnik_asymetrii_koszty)
+
+moment_centr_4_tmp <- 0
+for (xi in 1:25) {
+  moment_centr_4_tmp <- moment_centr_4_tmp + (data1[xi, "koszty"] - mean(data1[['koszty']]))^4
+}
+
+moment_centr_4_koszty <- 1/24 * moment_centr_4_tmp
+
+kurtoza_koszty <- moment_centr_4_koszty / (odchylenie_koszty ^ 4)
+#                                                                 - 3
+#                                                                 "w starszych pracach"
+print(kurtoza_koszty)
+
